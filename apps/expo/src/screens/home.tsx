997320086216fe7d3cef51c0d1dce2085d0356db
Xiel7 <ericng402@gmail.com>
2022-10-04 17:20:44 +0800

    testing this repo

--- apps/expo/src/screens/home.tsx
+++ apps/expo/src/screens/home.tsx
@@ -61,7 +61,7 @@ export const HomeScreen = () => {
   return (
     <SafeAreaView>
       <View className="h-full w-full p-4">
-        <Text className="text-5xl font-bold mx-auto pb-2">
+        <Text className="text-5xl font-bold mx-auto pb-2 pt-5">
           Create <Text className="text-indigo-500">T3</Text> Turbo
         </Text>
 
